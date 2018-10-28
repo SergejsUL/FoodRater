ada77208ef167afbdfeb6377957b9d496ba09874
SergejsUL <39090869+SergejsUL@users.noreply.github.com>
2018-10-28 20:04:31 +0000

    created adapter and registered that in mainjava

--- foodrater/app/src/main/java/ie/ul/serge/foodrater/MainActivity.java
+++ foodrater/app/src/main/java/ie/ul/serge/foodrater/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -17,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        FoodAdapter foodAdapter = new FoodAdapter();
+        recyclerView.setAdapter(foodAdapter);
+
+        foodAdapter.addFood();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
